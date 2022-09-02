e73a7e0e03e182992dbaa6920068e78db734a7e4
alexismcode <alexismcode@gmail.com>
2022-09-02 15:21:44 -0500

    Add fifth class

---
+++ 01.knowingJavascript/05.strictMode/js/app.js
@@ -0,0 +1,16 @@
+"use strict"
+
+/*
+x = "2.1234";
+Uncaught ReferenceError: x is not defined 
+*/
+
+var x;
+x = "2.1234";
+
+/*
+var public = "Hola";
+Uncaught SyntaxError: Unexpected strict mode reserved word
+*/
+
+var publicData = "Hola";
