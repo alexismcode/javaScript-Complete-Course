9b981da7f893f2015ca31003bd8eb1992a5937e7
alexismcode <alexismcode@gmail.com>
2022-09-02 18:06:40 -0500

    Add first class

---
+++ 03.typeOfData/01.numbers/js/app.js
@@ -0,0 +1,15 @@
+"use strict"
+
+var age = 19;
+//19 + 5 = 24
+
+var value = "100";
+//"100" + 5 = 1005
+
+var newValue = Number(value);
+//newValue + 20 = 120
+
+
+//parseInt(); integer numbers
+//parseFloat(); decimal numbers
+
