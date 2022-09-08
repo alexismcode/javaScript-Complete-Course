5c8ec9df16fcf077bb977f84204be5e6fadfe9d2
alexismcode <alexismcode@gmail.com>
2022-09-08 16:34:44 -0500

    Add first class

---
+++ 04.operators/01.arithmeticOperators/js/app.js
@@ -0,0 +1,36 @@
+"use strict"
+
+//*** Arithmetic Operators 
+
+var valA = 10;
+var valB = 20;
+
+//ADDITION +
+var addition = valA + valB;
+console.log(`The sum of ${valA} + ${valB} is equal to: ${addition}`);
+
+//SUBTRACTION - 
+var subtraction = valA - valB;
+console.log(`The subtraction of ${valA} - ${valB} is equal to: ${subtraction}`);
+
+//MULTIPLICATION *
+var multiplication = valA * valB;
+console.log(`The multiplication of ${valA} x ${valB} is equal to: ${multiplication}`);
+
+//DIVISION /
+var division = valA / valB;
+console.log(`The division of ${valA} / ${valB} is equal to: ${division}`);
+
+//MODULE OR RESIDUE %
+var moduleR = valA % valB;
+console.log(`The module of ${valA} % ${valB} is equal to: ${moduleR}`);
+
+//INCREASE ++
+var increase = valA;
+increase++;
+console.log(`The increase of ${valA} is equal to: ${increase}`);
+
+//DECREMENT --
+var decrement = valA;
+decrement--;
+console.log(`The decrement of ${valA} is equal to: ${decrement}`);
