3071c906d1b42f8c2bd53bf0dbcc490570310eda
alexismcode <alexismcode@gmail.com>
2022-09-08 17:04:12 -0500

    Add second class

---
+++ 04.operators/02.relationalOperators/js/app.js
@@ -0,0 +1,32 @@
+"use strict"
+
+//*****************************
+//*** Arithmetic Operators 
+
+var valA = 10;
+var valB = 20;
+
+//GREATER THAN >
+var greaterThan = valA > valB;
+console.log(`Is ${valA} greater than ${valB}: ${greaterThan}`);
+
+//LESS THAN <
+var lessThan = valA < valB;
+console.log(`Is ${valA} less than ${valB}: ${lessThan}`);
+
+//GREATER THAN OR EQUAL TO >=
+var greaterThanOrEqualTo = valA >= valB;
+console.log(`Is ${valA} greater than or equeal to ${valB}: ${greaterThanOrEqualTo}`);
+
+//LESS THAN OR EQUAL TO <=
+var lessThanOrEqualTo = valA <= valB;
+console.log(`Is ${valA} less than ${valB}: ${lessThanOrEqualTo}`);
+
+//EQUAL ==
+var equal = valA == valB;
+console.log(`Is ${valA} equal to ${valB}: ${equal}`);
+
+//NOT THE SAME OR DIFFERENT THAN !=
+var notTheSame = valA != valB;
+console.log(`Is ${valA} no the same or different than ${valB}: ${notTheSame}`);
+
