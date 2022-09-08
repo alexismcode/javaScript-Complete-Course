0e284421b53d4d7a07da4ead8c987b09733c2182
alexismcode <alexismcode@gmail.com>
2022-09-07 19:10:07 -0500

    Add fourth class

---
+++ 03.typeOfData/04.dates/js/app.js
@@ -0,0 +1,9 @@
+"use strict"
+
+var date = new Date();
+
+console.log(date.getDay());
+console.log(date.getDate());
+console.log(date.getHours());
+console.log(date.setDate(5));
+console.log(date.setDate(10));
